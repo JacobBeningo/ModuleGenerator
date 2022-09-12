c676bf5e5e9c334df6a5b7724c34851a3662a2a6
Jacob Beningo <jacob@beningo.com>
2022-09-12 16:37:58 -0400

    Fixed issue with creating a directory. Removed extra ModuleGenerator.py file.

--- ModuleGenerator.py
+++ ModuleGenerator.py
@@ -16,12 +16,15 @@ class ModuleGenerator:
 
     # Taken from https://stackoverflow.com/a/600612/119527
     def mkdir_p(self, path):
-        try:
-            os.makedirs(path)
-        except OSError as exc: # Python >2.5
-            if exc.errno == errno.EEXIST and os.path.isdir(path):
-                pass
-            else: raise
+        if path == '':
+            pass
+        else:
+            try:
+                os.makedirs(path)
+            except OSError as exc: # Python >2.5
+                if exc.errno == errno.EEXIST and os.path.isdir(path):
+                    pass
+                else: raise
 
     def WriteHeader(self):
         # Read in the .h template file
